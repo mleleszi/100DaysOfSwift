09aa5912dfb79bb1bd255ad57ca3e9a04c54e7a8
mleleszi <mleleszi@icloud.com>
2022-05-23 18:36:11 +0200

    project 9

---
+++ Project9/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Marcell Leleszi on 2022. 05. 22..
+//
+
+import Foundation
+
+struct Petiton: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
